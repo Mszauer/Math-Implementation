6d8c6c42c5fb7f4ec0c55c5eba2de1ad57cc5f8d
mszauer <mszauer@mail.usf.edu>
2015-12-30 10:56:48 -0800

    added overloads

--- Vector2/Program.cs
+++ Vector2/Program.cs
@@ -12,22 +12,47 @@ namespace Vector2 {
             Vector2 result = new Vector2(vector1.X + vector2.X, vector1.Y + vector2.Y);
             return result;
         }
+        public void Addition(Vector2 vector) {
+            X += vector.X;
+            Y += vector.Y;
+        }
         public Vector2 Subtraction(Vector2 vector1, Vector2 vector2) {
             Vector2 result = new Vector2(vector1.X - vector2.X, vector1.Y - vector2.Y);
             return result;
+        }
+        public void Subtraction(Vector2 vector) {
+            X -= vector.X;
+            Y -= vector.Y;
         }
         public Vector2 ScalarMultiplication(Vector2 vector, float scale) {
             Vector2 result = new Vector2(vector.X * scale, vector.Y * scale);
             return result;
         }
+        public void ScalarMultiplication(float scale) {
+            X *= scale;
+            Y *= scale;
+        }
         public Vector2 ScalarDivision(Vector2 vector, float scale) {
             Vector2 result = new Vector2(vector.X/scale,vector.Y/scale);
             return result;
+        }
+        public void ScalarDivision(float scale) {
+            X /= scale;
+            Y /= scale;
         }
         public float Dot(Vector2 vector1, Vector2 vector2) {
             float xComponent = vector1.X * vector2.X;
             float yComponent = vector1.Y * vector2.Y;
             return xComponent + yComponent;
+        }
+        public float Dot(float vector1,float vector2) {
+            return vector1 * vector2;
+        }
+        public float Length(Vector2 vector) {
+            double length = 0.0;
+            length = (double)Dot(vector, vector);
+            float result = (float)Math.Sqrt(length);
+            return result;
         }
         public float Length() {
             double length = 0.0;
@@ -39,6 +64,22 @@ namespace Vector2 {
             vector.X /= Length();
             vector.Y /= Length();
             return new Vector2(vector.X, vector.Y);
+        }
+        public void Normalize() {
+            X /= Length();
+            Y /= Length();
+        }
+        public float AngleBetween(Vector2 vector) {
+            double angle = 0.0;
+            double cosAngle = (double)(Dot(this, vector)) / (double)(Dot(Length(), vector.Length()));
+            angle = Math.Acos(cosAngle);
+            return (float)angle;
+        }
+        public float AngleBetween(Vector2 vector1, Vector2 vector2) {
+            double angle = 0.0;
+            double cosAngle = (double)(Dot(vector1, vector2)) / (double)(Dot(vector1.Length(),vector2.Length()));
+            angle = Math.Acos(cosAngle);
+            return (float)angle;
         }
     }
 

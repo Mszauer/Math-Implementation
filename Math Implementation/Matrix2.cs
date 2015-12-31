72e2c42f7077e6e0d46eedd71e2436d0d7920ef1
mszauer <mszauer@mail.usf.edu>
2015-12-31 14:21:56 -0800

    finished divide

--- Math Implementation/Matrix2.cs
+++ Math Implementation/Matrix2.cs
@@ -98,14 +98,6 @@ namespace Math_Implementation {
             result[1, 1] = (matrix1[0, 1] * matrix2[1, 0]) + (matrix1[1, 1] * matrix2[1, 1]);
             return result;
         }
-        public Matrix2 Multiply(Matrix2 matrix2) {
-            Matrix2 result = new Matrix2();
-            result[0, 0] = (this[0, 0] * matrix2[0, 0]) + (this[0, 1] * matrix2[1, 0]);
-            result[0, 1] = (this[0, 0] * matrix2[1, 0]) + (this[0, 1] * matrix2[1, 1]);
-            result[1, 0] = (this[0, 1] * matrix2[0, 0]) + (this[1, 1] * matrix2[0, 1]);
-            result[1, 1] = (this[0, 1] * matrix2[1, 0]) + (this[1, 1] * matrix2[1, 1]);
-            return result;
-        }
         public void Multiply(Matrix2 matrix) {
             this[0, 0] = (this[0, 0] * matrix[0, 0]) + (this[0, 1] * matrix[1, 0]);
             this[0, 1] = (this[0, 0] * matrix[1, 0]) + (this[0, 1] * matrix[1, 1]);
@@ -118,6 +110,29 @@ namespace Math_Implementation {
             result[0, 1] = (matrix1[0, 0] * matrix2[1, 0]) + (matrix1[0, 1] * matrix2[1, 1]);
             result[1, 0] = (matrix1[0, 1] * matrix2[0, 0]) + (matrix1[1, 1] * matrix2[0, 1]);
             result[1, 1] = (matrix1[0, 1] * matrix2[1, 0]) + (matrix1[1, 1] * matrix2[1, 1]);
+            return result;
+        }
+        public static Matrix2 Divide(Matrix2 matrix1,Matrix2 matrix2) {
+            Matrix2 result = new Matrix2();
+            result[0, 0] = matrix1[0, 0] / matrix2[0, 0];
+            result[0, 1] = matrix1[0, 1] / matrix2[0, 1];
+            result[1, 0] = matrix1[1, 0] / matrix2[1, 0];
+            result[1, 1] = matrix1[1, 1] / matrix2[1, 1];
+            return result;
+        }
+        public  void Divide(Matrix2 matrix) {
+            Matrix2 result = new Matrix2();
+            this[0, 0] /= matrix[0, 0];
+            this[0, 1] /= matrix[0, 1];
+            this[1, 0] /= matrix[1, 0];
+            this[1, 1] /= matrix[1, 1];
+        }
+        public static Matrix2 operator /(Matrix2 matrix1, Matrix2 matrix2) {
+            Matrix2 result = new Matrix2();
+            result[0, 0] = matrix1[0, 0] / matrix2[0, 0];
+            result[0, 1] = matrix1[0, 1] / matrix2[0, 1];
+            result[1, 0] = matrix1[1, 0] / matrix2[1, 0];
+            result[1, 1] = matrix1[1, 1] / matrix2[1, 1];
             return result;
         }
     }

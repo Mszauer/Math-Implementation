6e1923800cd9c50159a37a4a077905d043c6ef04
mszauer <mszauer@mail.usf.edu>
2015-12-31 14:04:54 -0800

    refactored code, corrected indexing

--- Math Implementation/Matrix2.cs
+++ Math Implementation/Matrix2.cs
@@ -3,11 +3,6 @@
 namespace Math_Implementation {
     class Matrix2 {
         public float[] Matrix;
-        public int Length {
-            get {
-                return Matrix.Length;
-            }
-        }
         public float this[int i] {
             get {
                 return Matrix[i];
@@ -16,10 +11,19 @@ namespace Math_Implementation {
                 Matrix[i] = value;
             }
         }
-        public Matrix2() {
-            Matrix = new float[2 * 2];
+        public float this[int i,int j] {
+            get {
+                return Matrix[(i * 2) + j];
+            }
+            set {
+                Matrix[(i * 2) + j] = value;
+            }
         }
-        public Matrix2(int[] values) {
+        public Matrix2() {
+            Matrix = new float[] { 1, 0,
+                                   0, 1 };
+        }
+        public Matrix2(params int[] values) {
             Matrix = new float[values.Length];
         }
 
@@ -27,13 +31,13 @@ namespace Math_Implementation {
             return Matrix[row * Matrix.Length + col];
         }
         public static float GetValue(Matrix2 matrix,int row, int col) {
-            return matrix.Matrix[row * matrix.Length + col];
+            return matrix.Matrix[row * 2 + col];
         }
         public static Matrix2 Add(Matrix2 matrix1, Matrix2 matrix2) {
             Matrix2 result = new Matrix2();
             for (int row = 0; row < 2; row++) {
                 for (int col = 0; col < 2; col++) {
-                    result[row * result.Length + col] = matrix1[row * matrix1.Length + col] + matrix2[row * matrix2.Length + col];
+                    result[row,col] = matrix1[row ,col] + matrix2[row ,col];
                 }
             }
             return result;
@@ -41,7 +45,7 @@ namespace Math_Implementation {
         public void Add(Matrix2 matrix) {
             for (int row = 0; row < 2; row++) {
                 for (int col = 0; col < 2; col++) {
-                    Matrix[row * Matrix.Length + col] = matrix[row * matrix.Length + col] + Matrix[row * Matrix.Length + col];
+                    this[row,col] = matrix[row,col] + this[row,col];
                 }
             }
         }
@@ -49,7 +53,7 @@ namespace Math_Implementation {
             Matrix2 result = new Matrix2();
             for (int row = 0; row < 2; row++) {
                 for (int col = 0; col < 2; col++) {
-                    result[row * result.Length + col] = matrix1[row * matrix1.Length + col] + matrix2[row * matrix2.Length + col];
+                    result[row,col] = matrix1[row,col] + matrix2[row ,col];
                 }
             }
             return result;
@@ -58,7 +62,7 @@ namespace Math_Implementation {
             Matrix2 result = new Matrix2();
             for (int row = 0; row < 2; row++) {
                 for (int col = 0; col < 2; col++) {
-                    result[row * result.Length + col] = matrix1[row * matrix1.Length + col] - matrix2[row * matrix2.Length + col];
+                    result[row,col] = matrix1[row,col] - matrix2[row,col];
                 }
             }
             return result;
@@ -66,7 +70,7 @@ namespace Math_Implementation {
         public void Subtract(Matrix2 matrix) {
             for (int row = 0; row < 2; row++) {
                 for (int col = 0; col < 2; col++) {
-                    Matrix[row * Matrix.Length + col] = matrix[row * matrix.Length + col] - Matrix[row * Matrix.Length + col];
+                    this[row,col] = matrix[row,col] - this[row ,col];
                 }
             }
         }
@@ -74,17 +78,14 @@ namespace Math_Implementation {
             Matrix2 result = new Matrix2();
             for (int row = 0; row < 2; row++) {
                 for (int col = 0; col < 2; col++) {
-                    result[row * result.Length + col] = matrix1[row * matrix1.Length + col] - matrix2[row * matrix2.Length + col];
+                    result[row,col] = matrix1[row,col] - matrix2[row,col];
                 }
             }
             return result;
         }
         public static Matrix2 Multiply(Matrix2 matrix1, Matrix2 matrix2) {
             Matrix2 result = new Matrix2();
-            result[0] =( matrix1[0] * matrix2[0]) + (matrix1[1]*matrix2[2]);
-            result[1] = (matrix1[0] * matrix2[1]) + (matrix1[1]*matrix2[3]);
-            result[2] = (matrix1[2] * matrix2[2]) + (matrix1[3]*matrix2[1]);
-            result[3] = (matrix1[2] * matrix2[1]) + (matrix1[3]*matrix2[3]);
+            result[0] =( matrix1[0,0] * matrix2[0,0]) + (matrix1[0,1]*matrix2[1,0]);
             return result;
         }
     }

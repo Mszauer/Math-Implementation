f1523db8aa04be0d2033173ee0dcdf1fdd407485
mszauer <mszauer@mail.usf.edu>
2016-01-06 11:39:36 -0800

    added toMatrix and angleaxis

--- Math Implementation/Quaternion.cs
+++ Math Implementation/Quaternion.cs
@@ -30,6 +30,43 @@ namespace Math_Implementation {
             result.W /= qLength;
             return result;
         }
+        public static Quaternion FromAngleAxis(Vector3 axis, float angle) {
+            angle = angle * (float)(Math.PI / 180);
+            Quaternion result = new Quaternion();
+            float sin = (float)Math.Sin(angle);
+            axis = Vector3.Normalize(axis);
+            result.W = (float)Math.Cos(angle);
+            result.X = axis.X * sin;
+            result.Y = axis.Y * sin;
+            result.Z = axis.Z * sin;
+            return result;
+        }
+        public static Matrix3 ToMatrix(Quaternion q) {
+            Matrix3 result = new Matrix3();
+            float xSq = q.X * q.X;
+            float ySq = q.Y * q.Y;
+            float zSq = q.Z * q.Z;
+            float wSq = q.W * q.W;
+            float twoX = 2.0f * q.X;
+            float twoY = 2.0f * q.Y;
+            float twoW = 2.0f * q.W;
+            float xy = twoX * q.Y;
+            float xz = twoX * q.Z;
+            float yz = twoY * q.Z;
+            float wx = twoW * q.X;
+            float wy = twoW * q.Y;
+            float wz = twoW * q.Z;
+            result[0, 0] = wSq + xSq - ySq - zSq;
+            result[0, 1] = xy - wz;
+            result[0, 2] = xz + wy;
+            result[1, 0] = xy + wz;
+            result[1, 1] = wSq - xSq + ySq - zSq;
+            result[1, 2] = yz - wx;
+            result[2, 0] = xz - wy;
+            result[2, 1] = yz + wx;
+            result[2, 2] = wSq - xSq - ySq + zSq;
+            return result;
+        }
         public static Quaternion Conjugate(Quaternion q) {
             Quaternion result = new Quaternion();
             result.X = -q.X;
